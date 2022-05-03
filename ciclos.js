185876fe94f705a3e354860c49d070448cb24ec8
Javier <javi19mono@gmail.com>
2022-05-03 13:11:40 -0500

    examen

---
+++ ciclos.js
@@ -0,0 +1,6 @@
+var CantSaludos = 0;
+ 
+while(CantSaludos < 5){
+    console.log("Hola ");
+    CantSaludos += 1;
+}
---
+++ evaluacionCondicionales.js
@@ -0,0 +1,81 @@
+// javier zapata angela delgado
+
+
+//1
+var ValorVariable= 350;
+ 
+if (ValorVariable > 300 ){
+   console.log("La condición es verdadera")
+}
+
+//2
+var valor= 20;
+
+if (valor >= 20 || valor >= 5){
+   console.log("El valor es correcto")
+}
+//3
+var numero= 40;
+ 
+if (numero===40){
+   console.log('El número es igual que "40" en valor y tipo');
+}else if(numero>10||numero<50){
+    console.log('El número es mayor que 10 y menor a 50');
+}else if(numero>=9 && numero<=8){
+    console.log("El número es igual que 9 o menor igual que 8");
+}else if(numero>=50){
+    console.log("El número es mayor igual que 50");
+}
+//Respuesta 3.A
+    numero= 8 
+    if (numero>4){
+    console.log("el valor es correcto")
+    }
+//Respuesta 3.B
+    numero= 50
+    if(numero<100){
+        console.log("el valor es estrictamente menor")
+    }
+//Respuesta 3.C
+    numero= 5.0
+    if(numero===5.0){
+        console.log("el valor es igual en numero y en tipo")
+    }
+//Respuesta 3.D
+    numero=32
+    if(numerito>30 && numerito<100){
+        console.log("el numero si esta entre 30 y 100")
+    }
+    //4
+var numeroA ="5"
+var numeroO= "5"
+if(numeroA==numeroO){
+    console.log("son iguales")
+}else{
+console.log(numeroA+numeroO)
+}
+//5
+var persona="infante"
+if(persona=="infante"){
+    console.log("se le obsequia un juguete")
+}else if(persona=="joven"){ 
+    console.log("se le obsequia unos audifonos")
+}else if(persona=="adulto") { 
+    console.log("se le obsequia bono para cena en pareja")
+}else if(persona=="adulto mayor"){
+    console.log("se le obsequia un paseo a sitio turistico")
+}
+//6
+var tv="37pulgadas";
+  if(tv=="37pilgadas"){
+    console.log("este cuesta 500.000")
+}else if(tv=="42pulgadas"){
+  console.log("este cuesta 820.000")
+}else if(tv=="55pulgadas"){
+console.log("este cuesta 1’500.000")
+}else if(tv=="75pulgadas"){
+    console.log("este cuesta 3’000.000")
+}else{
+    console.log("el que usted busca no existe en nuestra tienda")
+}
+
